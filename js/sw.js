3b3b5c2dace8c41fd3989c610fb179597915d3e2
larilura <tailfox.larilura@gmail.com>
2018-07-09 20:45:59 +0900

    v
    v

--- js/sw.js
+++ js/sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/5.2.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/5.2.0/firebase-messaging.js');
 
 var config = {
     apiKey: "AIzaSyD1KiLXHweGyxO0z5154e07nivRTWoZba8",
